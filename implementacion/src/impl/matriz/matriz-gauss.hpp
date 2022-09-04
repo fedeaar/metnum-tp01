b5a4cb89be0c95e7f334dc587cd4953589ff7cef
natanvek <natanvek11@gmail.com>
2022-09-04 14:55:31 -0300

    Update matriz-gauss.hpp

--- implementacion/src/impl/matriz/matriz-gauss.hpp
+++ implementacion/src/impl/matriz/matriz-gauss.hpp
@@ -58,36 +58,37 @@ matriz<T, R> matriz<T, R>::gauss_elim(vector<T> &b) const {
     /** pre: A_ii != 0 para i: 0 ... N y b.size() == N. */
     assert(b.size() == n());
     matriz res {*this};
+    auto A = &(res._rep._mat);
     for (size_t i = 0; i < n()- 1; i++) {
-        T mii = res._rep._mat[i][0].val;
+        T mii = (*A)[i][0].val;
         for (size_t j = i + 1; j < n(); j++) {
-            typename alt<T>::elem col_i = res._rep._mat[j][0];
-            if(col_i.col != i) continue;
-            T mij = col_i.val / mii;
+            typename alt<T>::elem* col_i = &((*A)[j][0]);
+            if(col_i->col != i) continue;
+            T mij = col_i->val / mii;
             size_t pos = 1;
             vector<typename alt<T>::elem> newRow = {};
-            for (size_t k = 1; k < res._rep._mat[i].size(); k++) {
-                while(pos < res._rep._mat[j].size() && res._rep._mat[j][pos].col < res._rep._mat[i][k].col) {
-                    if(abs(res._rep._mat[j][pos].val) > 1e-4) newRow.push_back(res._rep._mat[j][pos]);
+            for (size_t k = 1; k < (*A)[i].size(); k++) {
+                while(pos < (*A)[j].size() && (*A)[j][pos].col < (*A)[i][k].col) {
+                    if(abs((*A)[j][pos].val) > 1e-4) newRow.push_back((*A)[j][pos]);
                     pos++;
                 }
 
                 // m[j][pos].col >= m[k].col
                 typename alt<T>::elem x;
-                if(pos < res._rep._mat[j].size() && res._rep._mat[j][pos].col == res._rep._mat[i][k].col) {
-                    x.val = res._rep._mat[j][pos].val;
+                if(pos < (*A)[j].size() && (*A)[j][pos].col == (*A)[i][k].col) {
+                    x.val = (*A)[j][pos].val;
                     pos++;
                 }
-                x.val -= mij * res._rep._mat[i][k].val;
-                x.col = res._rep._mat[i][k].col;
+                x.val -= mij * (*A)[i][k].val;
+                x.col = (*A)[i][k].col;
                 if(abs(x.val) > 1e-4) newRow.push_back(x);
             }
 
-            for(size_t k = pos; k < res._rep._mat[j].size(); ++k) {
-                if(abs(res._rep._mat[j][k].val) > 1e-4) newRow.push_back(res._rep._mat[j][k]);
+            for(size_t k = pos; k < (*A)[j].size(); ++k) {
+                if(abs((*A)[j][k].val) > 1e-4) newRow.push_back((*A)[j][k]);
             }
 
-            res._rep._mat[j] = newRow;
+            (*A)[j] = newRow;
 
             b[j] = b[j] - mij * b[i];
         }
