0a7833d04a4e03210b3d1d50681f2b6b2e75eaf3
fedeaar <fa.arienti@gmail.com>
2022-09-05 08:40:04 -0300

    update IO, ahora guarda mejor el resultado

--- implementacion/main.cpp
+++ implementacion/main.cpp
@@ -12,8 +12,23 @@ int main(int argc,  char** argv) {
         return -1;
     }
 
-    // params fijos
+    // file loc
     string in = argv[1];
+    // file name
+    string file_name;
+    auto delim = in.find_last_of('/');
+    if (delim != string::npos) {
+        file_name = in.substr(delim + 1, in.size());
+    } else {
+        file_name = in;
+    }
+    // remover extension del nombre
+    delim = file_name.find_last_of('.');
+    if (delim != string::npos) {
+        file_name = file_name.substr(0, delim);
+    }
+
+    // p_val
     double p_val {};
     IO::stodcast(argv[2], p_val, "error: el valor p no se pudo interpretar como double.");
 
@@ -33,10 +48,12 @@ int main(int argc,  char** argv) {
         }
         args[param] = val;
     }
+    // out_dir
     string outdir = args.count("-out") ? args.at("-out") : "./";
     if (outdir[outdir.length() - 1] != '/') {
         outdir += '/';
     }
+    // debug flag
     bool debug = args.count("-debug");
 
     // exe
@@ -46,15 +63,15 @@ int main(int argc,  char** argv) {
     cout << "creando matriz I - pWD...\n";
     matriz<alt> mat = pagerank::make<alt>(data);
     if (debug) {
-        string matriz_out = outdir + "matriz.txt";
-        cout << "--DEBUG-- guardando matriz en: " + matriz_out << " (si es una direccion valida)." << endl;
+        string matriz_out = outdir + file_name + "_debug.out";
+        cout << "-debug- guardando matriz en: " + matriz_out << " (si el path existe)." << endl;
         IO::pagerank_write_matriz(matriz_out, mat);
     }
 
     cout << "calculando PageRank...\n";
     vector<double> solucion = pagerank::solve(mat);
-    string out = outdir + "resultado.txt";
-    cout << "guardando resultado: " + out << " (si es una direccion valida)." << endl;
+    string out = outdir + file_name + ".out";
+    cout << "guardando resultado: " + out << " (si el path existe).";
     IO::pagerank_write_out(out, p_val, solucion);
 
     return 0;
