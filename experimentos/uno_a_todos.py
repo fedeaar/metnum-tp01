0547b779c186eb07c7954b7a4b5006b3083301d2
fedeaar <fa.arienti@gmail.com>
2022-09-09 15:28:39 -0300

    merge changes uno_a_todos.py

--- experimentos/uno_a_todos.py
+++ experimentos/uno_a_todos.py
@@ -1,9 +1,10 @@
+import pagerank_scripts.IO as IO
+import pagerank_scripts.utils as utils
+
 import numpy as np
-import base
 
-IO = base.IO
+
 print('\n')
-
 def uno_a_todos(n, p = 1 - 1e-4):
     pathIn, pathOut, pathRes = base.createInOut("uno_a_todos")
     typeIn = ".txt"
@@ -17,13 +18,15 @@ def uno_a_todos(n, p = 1 - 1e-4):
         half = int(np.floor(i/2))
         W[half] = W[half] + 1
 
-        IO.run(W, p, filename= pathIn + experiment + typeIn, out_dir= pathOut, debug=False)
+        IO.createFileIn(pathIn + experiment + typeIn, W)
+        IO.run(pathIn + experiment + typeIn, p, out_dir=pathOut)
+
         p, solucion = IO.readFileOut(filename=pathOut + experiment + typeOut)
 
         resultFile.write(str(solucion[0]) + '\n')
         result.append(solucion[0])
 
     resultFile.close()
-    base.plot(1, n + 1, result)
+    utils.plot(1, n + 1, result)
 
 uno_a_todos(100)
