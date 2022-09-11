05585527375337b6e9322eeb96ef7e3b431719fe
fedeaar <fa.arienti@gmail.com>
2022-09-11 15:14:42 -0300

    cosas

--- experimentos/todos_a_uno.py
+++ experimentos/todos_a_uno.py
@@ -4,19 +4,18 @@ import pagerank_scripts.utils as utils
 import numpy as np
 
 
-print('\n')
-
-def todos_a_uno(n, p = 1 - 1e-4):
+def todos_a_uno(n, p=0.85):
     pathIn, pathOut, pathRes = IO.createInOut("todos_a_uno")
     typeIn = ".txt"
     typeOut = ".out"
     resultFile = open(pathRes + "res.txt", "w")
     result = []
+    result1 = []
     for i in range(1, n + 1):
         experiment = "t_"+ str(i)
  
         W = np.zeros((n,n))
-        W[1:i] = 1
+        W[0, 1:i] = 1
 
         IO.createFileIn(pathIn + experiment + typeIn, W)
         IO.run(pathIn + experiment + typeIn, p, out_dir=pathOut)
@@ -24,8 +23,15 @@ def todos_a_uno(n, p = 1 - 1e-4):
 
         resultFile.write(str(solucion[0]) + '\n')
         result.append(solucion[0])
+        result1.append(solucion[1])
 
     resultFile.close()
-    utils.plot(1, n + 1, result)
+    utils.graficar(
+        x=[x for x in range(1, n + 1)]*2, 
+        y=result + result1,
+        hue=["nodo 'uno'"]*n + ["nodo testigo"]*n,
+        xaxis="cantidad de referencias",
+        yaxis="puntaje",
+        filename=pathRes+"grafico.png")
 
 todos_a_uno(100)
